ad817aea25e443c3b0dcdb311b2bb3fce5a0d0e2
Shyam-45 <shyam14027@gmail.com>
2025-05-19 00:06:30 +0530

    Implemented Authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1>Welcome to Online Judge</h1>
+      <h1>Welcome to the Online Judge</h1>
     </>
   );
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bcrypt from "bcryptjs";
-import { createRequire } from 'module';
+import {createRequire } from 'module';
 import User from '../models/User.js';
 
 const require = createRequire(import.meta.url);
