04d34909ca4559bb3bbf057cc98ce8634ab60713
Shyam-45 <shyam14027@gmail.com>
2025-05-26 01:52:39 +0530

    Execution logic for Java

---
+++ server/utils/executeJava.js
@@ -0,0 +1,22 @@
+import { exec as execCallback } from "child_process";
+import { promisify } from "util";
+
+const exec = promisify(execCallback);
+
+export const executeJava = async (filePath) => {
+  const command = `java ${filePath}`;
+
+  try {
+    const { stdout, stderr } = await exec(command);
+
+    if (stderr) {
+      console.error(`Standard error (warnings or errors): ${stderr}`);
+      return { success: false, error: stderr };
+    }
+
+    return { success: true, message: stdout };
+  } catch (err) {
+    console.error(`Code Execution failed: ${err.message}`);
+    return { success: false, error: err.message };
+  }
+};
