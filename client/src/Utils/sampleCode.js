66b491ee3f975a63d67aea4fa20a6ad960cf4a80
Shyam-45 <shyam14027@gmail.com>
2025-05-26 01:41:45 +0530

    Added sample codes

---
+++ client/src/Utils/sampleCode.js
@@ -0,0 +1,38 @@
+export const pySample = `# Print "Hello World"
+print("Hello, World!")`;
+
+export const jsSample = `// Print "Hello world"
+console.log("Hello, World!");`;
+
+export const cSample = `// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // Writing print statement to print hello world
+    printf("Hello World");
+
+    return 0;
+}`;
+
+export const cppSample = `// Necessary header files for input output functions
+#include <iostream>
+using namespace std;
+
+// main() function: where the execution of
+// C++ program begins
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World";
+
+    return 0;
+}`;
+
+export const javaSample = `// Print "Hello world"
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}`;
