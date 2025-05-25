6bb8f2a05419b1723ede762a7962f0ba1169820b
Shyam-45 <shyam14027@gmail.com>
2025-05-26 01:52:29 +0530

    Execution logic for JS

---
+++ server/utils/executeJs.js
@@ -0,0 +1,22 @@
+import { exec as execCallback } from "child_process";
+import { promisify } from "util";
+
+const exec = promisify(execCallback);
+
+export const executeJs = async (filePath) => {
+  const command = `node ${filePath}`;
+
+  try {
+    const { stdout, stderr } = await exec(command);
+
+    if (stderr) {
+      console.error(`Standard error (warnings or errors): ${stderr}`);
+      return { success: false, error: stderr };
+    }
+
+    return { success: true, message: stdout };
+  } catch (err) {
+    console.error(`Code Execution failed: ${err.message}`);
+    return { success: false, error: err.message };
+  }
+};
