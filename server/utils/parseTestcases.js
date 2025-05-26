cb72131f541aeab7f06f98f78a658055afd2ad5e
Shyam-45 <shyam14027@gmail.com>
2025-05-27 05:25:36 +0530

    Helper function to store test cases

---
+++ server/utils/parseTestcases.js
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+
+export function parseTestCases(inputPath, outputPath) {
+  const inputStr  = fs.readFileSync(inputPath,  "utf8");
+  const outputStr = fs.readFileSync(outputPath, "utf8");
+
+  const inputLines  = inputStr.split(/\r?\n/);
+  const outputLines = outputStr.split(/\r?\n/);
+
+  const numBlocks = parseInt(inputLines[0], 10);
+  let inIdx  = 1;
+  let outIdx = 0;
+  const result = [];
+
+  for (let b = 0; b < numBlocks; b++) {
+    const blockLen = parseInt(inputLines[inIdx], 10);
+
+    // slice out the original block lines
+    const blockLines = inputLines.slice(inIdx + 1, inIdx + 1 + blockLen);
+    // now prefix the size
+    const inputBlock = [blockLen, ...blockLines].join("\n");
+
+    const outputBlock = outputLines
+      .slice(outIdx, outIdx + blockLen)
+      .join("\n");
+
+    result.push({
+      iofID:             uuidv4(),
+      inputFile:    inputBlock,
+      outputFile:   outputBlock,
+      // size:               blockLen,
+    });
+
+    inIdx  += 1 + blockLen;
+    outIdx += blockLen;
+  }
+
+  return result;
+}
