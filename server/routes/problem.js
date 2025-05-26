353f8dc16df1ba055093d967cdb2715179543085
Shyam-45 <shyam14027@gmail.com>
2025-05-27 05:24:19 +0530

    added route to handle submission

--- server/routes/problem.js
+++ server/routes/problem.js
@@ -1,21 +1,48 @@
 import express from "express";
-import {showProblemList, showProblem, createProblem, deleteProblem} from "../controllers/problem.js"
-import { codeOutput } from "../controllers/submission.js";
+import multer from "multer";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import {
+  showProblemList,
+  showProblem,
+  createProblem,
+  deleteProblem,
+} from "../controllers/problem.js";
+import { codeOutput, codeVerdict } from "../controllers/submission.js";
 
 const router = express.Router();
 
 // Show all problems
 router.get("", showProblemList);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const uploadDir = path.join(__dirname, "../utils/uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+const upload = multer({ dest: uploadDir });
+
 // Create Problem route
-router.post("/newProblem", createProblem);
+router.post(
+  "/newProblem",
+  upload.fields([
+    { name: "sampleInputFile", maxCount: 1 },
+    { name: "sampleOutputFile", maxCount: 1 },
+    { name: "inputFile", maxCount: 1 },
+    { name: "outputFile", maxCount: 1 },
+  ]),
+  createProblem
+);
 
 // Run problem route
 router.post("/:problemID/run", codeOutput);
 
+// Submit problem route
+router.post("/:problemID/submit", codeVerdict);
+
 // Read & delete Problem route
-router.route("/:problemID")
-  .get(showProblem)
-  .delete(deleteProblem);
+router.route("/:problemID").get(showProblem).delete(deleteProblem);
 
 export default router;
