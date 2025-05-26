f7c31b2c74ec048ef6891f42ad73bc783e9bee46
Shyam-45 <shyam14027@gmail.com>
2025-05-27 05:12:45 +0530

    fixed bug related to userId

--- client/src/Contexts/AuthContext.jsx
+++ client/src/Contexts/AuthContext.jsx
@@ -4,9 +4,10 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
     const [isSigned, setIsSigned] = useState(false);
+    const [userId, setUserId] = useState("");
 
     return (
-    <AuthContext.Provider value={{ isSigned, setIsSigned }}>
+    <AuthContext.Provider value={{ isSigned, setIsSigned, userId, setUserId }}>
       {children}
     </AuthContext.Provider>
   );
