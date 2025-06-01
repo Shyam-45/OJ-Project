77e351cb315a0636966bf7368ec68aa5b644cdd1
Shyam-45 <shyam14027@gmail.com>
2025-06-02 00:28:54 +0530

    updated UI

--- client/src/Components/SolveProblem.jsx
+++ client/src/Components/SolveProblem.jsx
@@ -34,6 +34,11 @@ export default function SolveProblem() {
   const [verdict, setVerdict] = useState("");
   const [problemErr, setProblemErr] = useState("");
   const [outputErr, setOutputErr] = useState("");
+  const [customOutput, setCustomOutput] = useState(
+    `5
+2 5
+3 5`
+  );
 
   useEffect(() => {
     async function fetchProblem() {
@@ -150,155 +155,155 @@ export default function SolveProblem() {
         <div>{problemErr}</div>
       ) : (
         <div className="flex flex-col lg:flex-row justify-between m-4">
-          <div className="div1 w-2/5 flex flex-col">
-            <div className="flex justify-between">
-              <span className="text-3xl font-bold mt-8 px-4 dark:text-gray-300">
+          <div className="lg:w-2/5 flex flex-col pr-4">
+            <div className="flex justify-between items-center py-2 mt-8 bg-gray-100 dark:bg-gray-800">
+              <div className="text-2xl font-bold w-4/5 px-4 dark:text-gray-300">
                 {problem.title}
-              </span>
-              <span className="text-2xl dark:text-gray-300 mt-8 px-4 bg-gray-100 dark:bg-gray-700">
+              </div>
+              <span className="inline-flex items-center justify-center text-2xl mx-2 dark:text-gray-300 px-4 lg:bg-gray-200 lg:dark:bg-gray-700">
                 {problem.tags}
               </span>
             </div>
-            <div className="p-4">
-              <h3 className="font-medium text-2xl mt-4 dark:text-gray-300">Description</h3>
-              <p className="text-2xl mt-4 dark:text-gray-300">{problem.description}</p>
+            <div className="px-4 mt-4 bg-gray-100 dark:bg-gray-800">
+              <h3 className="font-medium text-lg pt-4 dark:text-gray-300">
+                Description
+              </h3>
+              <p className="text-xl py-2 dark:text-white">
+                {problem.description}
+              </p>
             </div>
-            <div className="p-4">
-              <h3 className="text-2xl mt-4 dark:text-gray-300">Input</h3>
-              <p className="text-2xl mt-4 dark:text-gray-300">{problem.inputInfo}</p>
+            <div className="px-4 mt-4 bg-gray-100 dark:bg-gray-800">
+              <h3 className="text-lg pt-4 font-medium dark:text-gray-300">
+                Input Format
+              </h3>
+              <p className="text-xl py-2 dark:text-gray-300">
+                {problem.inputInfo}
+              </p>
             </div>
-            <div className="p-4">
-              <h3 className="text-2xl mt-4 dark:text-gray-300">Output</h3>
-              <p className="text-2xl mt-4 dark:text-gray-300">{problem.outputInfo}</p>
+            <div className="px-4 mt-4 bg-gray-100 dark:bg-gray-800">
+              <h3 className="text-lg pt-4 font-medium dark:text-gray-300">
+                Output Format
+              </h3>
+              <p className="text-xl py-2 dark:text-gray-300">
+                {problem.outputInfo}
+              </p>
             </div>
-            <div className="input_output w-3/5">
-              <div className="flex justify-between items-center">
-                <span className="text-2xl w-1/2">Input</span>
-                <span className="text-2xl w-1/2">Output</span>
+            <div className="w-full mt-4 bg-gray-100 dark:bg-gray-800">
+              <div className="flex justify-between items-center px-4 mt-4 ">
+                <div className="text-xl font-medium w-1/2 dark:text-gray-300 ">
+                  Input
+                </div>
+                <div className="text-xl font-medium w-1/2 dark:text-gray-300">
+                  Output
+                </div>
               </div>
               {problem.sampleInputOutput.map((item) => (
-                <div key={item.sioID} className="flex justify-between items-center">
-                  {/* <h4 className="font-semibold">Input</h4> */}
-                  <pre className="">{item.input}</pre>
-                  {/* <h4 className="font-semibold">Output</h4> */}
-                  <pre className="">{item.output}</pre>
+                <div key={item.sioID} className="flex justify-between">
+                  <div className="w-1/2 px-4 my-4 text-lg dark:text-gray-300">
+                    <pre>{item.input}</pre>
+                  </div>
+                  <div className="w-1/2 px-4 my-4 text-lg dark:text-gray-300">
+                    <pre>{item.output}</pre>
+                  </div>
                 </div>
               ))}
             </div>
           </div>
-          <div className="w-3/5 flex flex-col">
-            <div className="compiler">compiler</div>
-            <div className="submissin_buttons">submission buttons</div>
+          <div className="lg:w-3/5 flex flex-col flex-1 h-screen dark:bg-gray-600">
+            <div className="flex flex-col pt-4 pr-4 mb-2">
+              <div className=" grid grid-cols-2 lg:grid-cols-4 mb-4 pl-4 bg-slate-100 rounded lg:rounded-full dark:bg-gray-800">
+                <button
+                  type="button"
+                  className="w-1/5 border-2 my-4 text-lg  bg-blue-600 text-white rounded-full pl-8 pr-16"
+                  onClick={handleRun}
+                >
+                  Run
+                </button>
+                <button
+                  type="button"
+                  className="w-1/5 border-2 my-4 text-lg  bg-green-700 text-white rounded-full pl-4 pr-20"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  className="disabled w-1/5 text-nowrap border-2 my-4 bg-purple-500 text-white text-lg rounded-full pl-4 pr-24"
+                >
+                  AI Review
+                </button>
+                <div className="mt-4 w-1/5">
+                  <select
+                    name="language"
+                    value={language}
+                    onChange={selectLanguage}
+                    className="mb-4 p-2 border-2 border-gray-300 rounded-full dark:bg-gray-400"
+                  >
+                    <option value="c">C</option>
+                    <option value="cpp">C++</option>
+                    <option value="py">Python</option>
+                    <option value="js">Javascript</option>
+                    <option value="java">Java</option>
+                  </select>
+                </div>
+              </div>
+              <div className="w-full h-[500px] overflow-auto border-red-400 border-4">
+                <Editor
+                  value={code}
+                  onValueChange={(code) => setCode(code)}
+                  highlight={highlightCode}
+                  padding={10}
+                  className="code-editor text-2xl  bg-gray-100 dark:bg-gray-800 dark:text-gray-300 w-full min-h-full"
+                />
+              </div>
+            </div>
+            <div className="mt-4 mr-4 bg-gray-100 dark:bg-gray-800">
+              {verdict && (
+                <p
+                  className={`ml-4 mt-4 text-2xl font-bold break-words whitespace-normal ${
+                    verdict === "FAIL"
+                      ? "text-red-500"
+                      : "text-green-800 dark:text-green-400"
+                  }`}
+                >
+                  {verdict}
+                </p>
+              )}
+              {outputMessage && (
+                <pre className="mt-2 p-4 text-xl rounded dark:text-gray-300 break-words whitespace-normal">
+                  {outputMessage}
+                </pre>
+              )}
+              {outputErr && (
+                <p className="mt-2 px-4 text-lg text-red-600 font-medium break-words whitespace-normal">
+                  {outputErr}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col lg:flex-row lg:justify-between pr-4 m-2 ">
+              <div className="lg:w-2/5 border-4 bg-slate-100 dark:bg-slate-700 dark:border-slate-500">
+                <h3 className="text-lg font-medium p-2 dark:text-gray-300">
+                  Custom Input
+                </h3>
+                <textarea
+                  rows={5}
+                  placeholder="Type your input here..."
+                  className="text-xl w-full resize-none rounded-lg pl-2 py-2 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-0"
+                />
+              </div>
+              <div className="h-4 lg:hidden"></div>
+              <div className="lg:w-1/2 border-4 bg-slate-100 dark:bg-slate-700 dark:border-slate-500">
+                <h3 className="text-lg font-medium p-2 dark:text-gray-300">
+                  Output
+                </h3>
+                <pre className="px-4 my-4 text-xl break-words dark:text-gray-300 ">
+                  {customOutput}
+                </pre>
+              </div>
+            </div>
           </div>
         </div>
       )}
     </>
   );
 }
-
-// <div className="flex flex-col md:flex-row">
-//   <div className="w-full md:w-1/2 p-6">
-//     <div className="bg-white rounded-lg shadow p-6 space-y-6">
-//       <div className="flex justify-between items-center">
-//         <h2 className="text-2xl font-bold text-gray-800">
-//           {problem.title}
-//         </h2>
-//         <span className="bg-gray-100 text-gray-800 px-3 py-1 rounded text-sm">
-//           {problem.tags}
-//         </span>
-//       </div>
-//       <div className="bg-gray-50 p-4 rounded">
-//         <h3 className="font-semibold mb-1">Description</h3>
-//         <p className="text-gray-600">{problem.description}</p>
-//       </div>
-//       <div className="bg-gray-50 p-4 rounded">
-//         <h3 className="font-semibold mb-1">Input</h3>
-//         <p className="text-gray-600">{problem.inputInfo}</p>
-//       </div>
-//       <div className="bg-gray-50 p-4 rounded">
-//         <h3 className="font-semibold mb-1">Output</h3>
-//         <p className="text-gray-600">{problem.outputInfo}</p>
-//       </div>
-//       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-//         {problem.sampleInputOutput.map((item) => (
-//           <div key={item.sioID} className="bg-gray-50 p-4 rounded">
-//             <h4 className="font-semibold">Input</h4>
-//             <pre className="text-gray-600 mb-2">{item.input}</pre>
-//             <h4 className="font-semibold">Output</h4>
-//             <pre className="text-gray-600">{item.output}</pre>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   </div>
-
-//   <div className="w-full md:w-1/2 p-6 flex flex-col">
-//     <div className="bg-white rounded-lg shadow p-6 flex flex-col h-full">
-//       <select
-//         name="language"
-//         value={language}
-//         onChange={selectLanguage}
-//         className="mb-4 p-2 border border-gray-300 rounded"
-//       >
-//         <option value="c">C</option>
-//         <option value="cpp">C++</option>
-//         <option value="py">Python</option>
-//         <option value="js">Javascript</option>
-//         <option value="java">Java</option>
-//       </select>
-
-//       <div className="flex-1 bg-gray-50 text-gray-800 p-4 rounded overflow-auto">
-//         <Editor
-//           value={code}
-//           onValueChange={(code) => setCode(code)}
-//           highlight={highlightCode}
-//           padding={10}
-//           style={{
-//             fontFamily: "Fira code, Fira Mono, monospace",
-//             fontSize: 20,
-//             color: "#1a202c",
-//             backgroundColor: "#f7fafc",
-//           }}
-//           className="w-full h-full"
-//         />
-//       </div>
-//       <div>
-//         <div className="mt-4 flex space-x-4">
-//           <button
-//             type="button"
-//             className="flex-1 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-//             onClick={handleRun}
-//           >
-//             Run
-//           </button>
-//           <button
-//             type="button"
-//             className="flex-1 bg-green-600 text-white py-2 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-//             onClick={handleSubmit}
-//           >
-//             Submit
-//           </button>
-//         </div>
-//         <div className="mt-4">
-//           {verdict && (
-//             <p
-//               className={`text-center text-xl font-bold ${
-//                 verdict === "FAIL" ? "text-red-500" : "text-green-600"
-//               }`}
-//             >
-//               {verdict}
-//             </p>
-//           )}
-//           {outputMessage && (
-//             <pre className="mt-2 p-4 bg-gray-100 rounded text-gray-800">
-//               {outputMessage}
-//             </pre>
-//           )}
-//           {outputErr && (
-//             <p className="mt-2 text-red-600 font-medium">{outputErr}</p>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
