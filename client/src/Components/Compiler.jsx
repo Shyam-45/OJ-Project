f92c94df269a49bd45efcf924d6ff706d6141837
Shyam-45 <shyam14027@gmail.com>
2025-06-05 07:35:01 +0530

    Separate compiler in navbar

--- client/src/Components/Compiler.jsx
+++ client/src/Components/Compiler.jsx
@@ -14,11 +14,13 @@ import {
   jsSample,
   cppSample,
 } from "../Utils/sampleCode.js";
+import { getCustomOutput } from "../Services/problem.js";
 
 export default function Compiler() {
   const [language, setLanguage] = useState("cpp");
   const [code, setCode] = useState(cppSample);
-  const [outputMessage, setOutputMessage] = useState("");
+  const [customInput, setCustomInput] = useState("");
+  // const [outputMessage, setOutputMessage] = useState("");
   const [outputErr, setOutputErr] = useState("");
   const [customOutput, setCustomOutput] = useState("5\n2 5\n3 5");
 
@@ -50,13 +52,41 @@ export default function Compiler() {
     if (!selected) return;
     setLanguage(selected);
     setCode(sampleCodeMap[selected]);
-    setOutputMessage("");
+    setCustomOutput("");
     setOutputErr("");
-    setVerdict("");
+    // setVerdict("");
+  };
+
+  const handleCustomInput = (event) => {
+    setCustomInput(event.target.value);
+    // console.log(event.target.value);
   };
 
   const handleCustomRun = async () => {
-    return;
+    const inputValue = (customInput || " ");
+    const payload = { language, code, inputValue};
+    try {
+      // setOutputMessage("");
+      setCustomOutput("");
+      setOutputErr("");
+      const response = await getCustomOutput(payload);
+      console.log(response);
+      if (!response.success) {
+        setOutputErr(response.error);
+        return;
+      }
+      // const { count, total } = response;
+      // let message = `All sample test cases passed`;
+      // if (count != total) {
+      //   message = `${count} out of ${total} sample test case passed`;
+      // }
+      setCustomOutput(response.message);
+      // setOutputMessage(message);
+      setOutputErr("");
+    } catch (error) {
+      console.log("Problem in receiving req from run {language, code}");
+      return;
+    }
   };
 
   return (
@@ -98,7 +128,8 @@ export default function Compiler() {
             </button>
             <button
               type="button"
-              className="text-xl py-2 my-2 px-4 rounded-full border-2 bg-indigo-600 hover:bg-indigo-700 text-white mx-2 dark:text-gray-200"
+              className="text-xl py-2 my-2 px-4 rounded-full border-2 bg-indigo-600 hover:bg-indigo-700 text-white mx-2 dark:text-gray-200 disabled:bg-gray-300"
+              disabled
               //   onClick={handleAIReview}
             >
               AI Review
@@ -113,6 +144,9 @@ export default function Compiler() {
             <textarea
               rows={10}
               placeholder="Type your input here..."
+              // name="customInput"
+              value={customInput}
+              onChange={handleCustomInput}
               className="text-xl w-full resize-none rounded-lg px-4 py-2 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-0"
             />
           </div>
@@ -123,6 +157,18 @@ export default function Compiler() {
             <pre className="px-4 my-4 text-xl break-words dark:text-gray-300 ">
               {customOutput}
             </pre>
+          </div>
+          <div className="mt-4 mr-4 bg-gray-100 dark:bg-gray-800">
+            {/* {outputMessage && (
+              <pre className="mt-2 p-4 text-xl rounded dark:text-gray-300 break-words whitespace-normal">
+                {outputMessage}
+              </pre>
+            )} */}
+            {outputErr && (
+              <p className="mt-2 px-4 text-lg text-red-600 font-medium break-words whitespace-normal">
+                {outputErr}
+              </p>
+            )}
           </div>
           <div className="cus_inp m-4 border-4 min-h-36 max-h-96 bg-slate-100 dark:bg-slate-700 dark:border-slate-500">
             <h3 className="text-lg font-medium p-2 break-words  dark:text-gray-300">
