e4ac6ed39ace220dadec5d6eda05b466d16332f7
Shyam-45 <shyam14027@gmail.com>
2025-06-05 07:42:21 +0530

    Initial commit

---
+++ client/src/Pages/ViewProblemPage.jsx
@@ -0,0 +1,9 @@
+import ViewProblem from "../Components/ViewProblem";
+
+export default function ViewProblemPage() {
+  return (
+    <div className="mt-4">
+        <ViewProblem />
+    </div>
+  );
+}
