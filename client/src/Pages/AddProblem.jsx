6ffd031064c77572702d9534eac6e8a4795617fa
Shyam-45 <shyam14027@gmail.com>
2025-06-05 07:41:44 +0530

    Initial commit

---
+++ client/src/Pages/AddProblem.jsx
@@ -0,0 +1,10 @@
+import NewProblemForm from "../Components/NewProblemForm";
+
+export default function AddProblem() {
+
+    return(
+        <div className="border-4">
+            <NewProblemForm />
+        </div>
+    )
+}
