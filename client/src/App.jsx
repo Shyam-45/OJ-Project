bfa681e0fc768a86203fd92ccfc4cbc72d2024bf
Shyam-45 <shyam14027@gmail.com>
2025-06-12 00:35:24 +0530

    UI updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./Components/Navbar.jsx";
-import Footer from "./Components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen dark:bg-gray-600">
       <Navbar />
-      <main className="flex-grow pt-16 bg">
+      <main className="flex-grow bg">
         <Outlet />
       </main>
       <Footer />
