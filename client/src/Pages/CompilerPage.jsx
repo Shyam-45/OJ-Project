8547535d3bd53e96f2f8952cc680524f5838445a
Shyam-45 <shyam14027@gmail.com>
2025-06-15 19:33:07 +0530

    Reorganized components

--- client/src/Pages/CompilerPage.jsx
+++ client/src/Pages/CompilerPage.jsx
@@ -1,7 +1,13 @@
+import Navbar from "../components/Navbar.jsx";
 import Compiler from "../components/Compiler.jsx";
 
 export default function CompilerPage() {
-    return(
+  return (
+    <div className="flex flex-col dark:bg-gray-600">
+      <Navbar />
+      <main className="mt-12 lg:mt-16 dark:bg-gray-600">
         <Compiler />
-    )
+      </main>
+    </div>
+  );
 }
