cf760af35494d468637aba8bdfbed5d756b2f89a
Shyam-45 <shyam14027@gmail.com>
2025-07-05 23:24:18 +0530

    Added route for bookmark

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import user from "./user.js";
 import problem from "./problem.js";
+import bookmark from "./bookmark.js";
 import { authentincateUser } from "../middlewares/authenticate.js";
 
 const router = express.Router();
@@ -8,6 +9,8 @@ const router = express.Router();
 router.use("/user", user);
 
 router.use("/problem", problem);
+
+router.use("/bookmark", bookmark);
 
 router.get("/authlogin", authentincateUser, (req, res) => {
   return res.status(200).json({ success: true, message: "User Authentication successful", userid: req.userId});
